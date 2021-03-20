1154652a1035cdbb008227d47d182bba01caab62
“mamoun95” <maamoon95@hotmail.com>
2021-03-20 04:54:38 +0200

    commit on 'main'

--- app.js
+++ app.js
@@ -1,21 +1,76 @@
 const express = require('express');
-const app = express();
-
-//register view engine
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+ //register app
+ const app = express();
+ //connect to database
+ const dbURI = "mongodb+srv://test:s5134747h@node.hkca4.mongodb.net/node?retryWrites=true&w=majority";
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err))
+ //register view engine
 app.set('views', './view');
 app.set('view engine', 'ejs');
-//listen for requist
-app.listen(3000);
+
+// sandbox and test creating blog in database
+app.get('/add-blog', (req, res) => {
+    //create constant for each blog
+    const blog = new Blog({
+        title: 'first blog',
+        snippet: 'this is first tested blog',
+        body: 'lorem ipsum and asduf ioausd casda'
+    });
+
+
+    //save blog in database
+    blog.save()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+app.get('/all-blog', (req, res) => {
+  
+    //fetch blog in database
+    Blog.find()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+})
+
+
+///////
+
+
+app.use(morgan('dev'));
+//middleware and static
 //app.use('/', express.static('client/home'));
 app.use(express.static('./view'));
 //get index
 
 app.get('/', (req, res) => {
-    const blogs = [{ title: 'how to start node', snippet: 'lorem ipsum jhsad liadsfu asdiu asdol' },{ title: 'why node is important', snippet: 'lorem ipsum jhsad liadsfu asdiu asdol' },{ title: 'playing is not important', snippet: 'lorem ipsum jhsad liadsfu asdiu asdol' },{ title: 'working is important', snippet: 'lorem ipsum jhsad liadsfu asdiu asdol' },];
+    console.log(req.url);
     var urrl = req.url;
     const active = '"active"';
+//fetch blog in database
+Blog.find()
+.then((result) => {
+    const blogs = result;
     res.render('index', { title: 'Home', blogs, active , urrl});
-    console.log(req.url);
+})
+.catch((err) => {
+    console.log(err);
+})
+    
+
+    
+    
     
 });
 //get about
